6bbf42099781650f1f5de4705d6d0fa7f387414e
Derry <thanhhaivevo@gmail.com>
2022-05-28 10:06:45 +0700

    faf

---
+++ js/service.js
@@ -0,0 +1,45 @@
+function laugugeus() {
+    document.getElementById("home").innerHTML = "Homepage";
+    document.getElementById("store").innerHTML = "Store";
+    document.getElementById("about").innerHTML = "About us";
+    document.getElementById("servies").innerHTML = "Service";
+    document.getElementById("addres").innerHTML = "55 To Giang, District 1, Ho Chi Minh City";
+
+    document.getElementById("q").innerHTML = "1. EXCHANGE POLICY";
+    document.getElementById("w").innerHTML = "Apply 01 time to exchange / 1 order";
+    document.getElementById("e").innerHTML = `– Products can be exchanged within 7 days from the date of purchase on the invoice (for customers buying directly at the store), 7 days from the date of receipt (For customers buying online)`;
+    document.getElementById("r").innerHTML = `– The product is new, with tags, tags and a purchase receipt, the product has not been washed and is not dirty, damaged by agents outside the store after purchase.`;
+    document.getElementById("h").innerHTML = "2. EXCHANGE POLICY FROM MANUFACTURERS AND REFUND";
+    document.getElementById("t").innerHTML = "– Products with technical defects: Products torn, glued, …";
+    document.getElementById("y").innerHTML = "3. EXCHANGE COST";
+    document.getElementById("u").innerHTML = `– Free exchange for customers purchased at MIKENCO in case of manufacturer defect, wrong delivery, damaged in transit`;
+    document.getElementById("i").innerHTML = `– In case the size does not fit or the customer does not like the product and does not want to receive the goods, please return the order to return the order (40,000 VND)`;
+}
+function laugugevn() {
+    document.getElementById("home").innerHTML = "Trang chủ";
+    document.getElementById("store").innerHTML = "Cửa hàng";
+    document.getElementById("about").innerHTML = "Về chúng tôi";
+    document.getElementById("servies").innerHTML = "Dịch vụ";
+    document.getElementById("addres").innerHTML = "55 Tô Giang, Quận 1, Thành phố hồ Chí Minh";
+
+    document.getElementById("q").innerHTML = "1. CHÍNH SÁCH ĐỔI HÀNG";
+    document.getElementById("w").innerHTML = "Áp dụng 01 lần đổi /1 đơn hàng";
+    document.getElementById("e").innerHTML = `– Sản phẩm đổi trong thời gian 7 ngày kể từ ngày mua hàng trên hoá đơn ( đối với khách mua hàng trực tiếp tại cửa hàng ), 7 ngày kể từ ngày nhận hàng ( Đối với khách mua trên online)`;
+    document.getElementById("r").innerHTML = `– Sản phẩm còn mới nguyên tem, tags và mang theo hoá đơn mua hàng, sản phẩm chưa giặt và không dơ
+    bẩn, hư hỏng bởi những tác nhân bên ngoài cửa hàng sau khi mua hàng`;
+    document.getElementById("h").innerHTML = "2. CHÍNH SÁCH ĐỔI DO LỖI TỪ NHÀ SẢN XUẤT VÀ HOÀN TIỀNy";
+    document.getElementById("t").innerHTML = "– Sản phẩm lỗi kỹ thuật: Sản phẩm rách, bung keo, …";
+    document.getElementById("y").innerHTML = "3. CHI PHÍ ĐỔI HÀNG";
+    document.getElementById("u").innerHTML = `– Miễn phí đổi hàng cho khách mua ở MIKENCO trong trường hợp bị lỗi từ nhà sản xuất, giao nhầm hàng, bị hư hỏng trong quá trình vận chuyển hàng`;
+    document.getElementById("i").innerHTML = `– Trong trường hợp không vừa size hay khách hàng không ưng sản phẩm không muốn nhận hàng phiền khách hàng trả ship hoàn đơn hàng về ( 40.000đ)`;
+
+}
+
+function themeBlack() {
+    document.getElementById('container').style.backgroundColor = 'black';
+
+}
+function themeGray() {
+    document.getElementById('container').style.backgroundColor = 'white';
+
+}
