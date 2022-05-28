8b49133e0d466b368a24130138e3c401f3810298
Derry <thanhhaivevo@gmail.com>
2022-05-28 10:31:28 +0700

    asdad

---
+++ js/shop.js
@@ -0,0 +1,52 @@
+function laugugeus() {
+    document.getElementById("home").innerHTML = "Homepage";
+    document.getElementById("store").innerHTML = "Store";
+    document.getElementById("about").innerHTML = "About us";
+    document.getElementById("servies").innerHTML = "Service";
+    document.getElementById("addres").innerHTML = "55 To Giang, District 1, Ho Chi Minh City";
+    document.getElementById("buynow").innerHTML = "BUY NOW";
+    document.getElementById("buynow1").innerHTML = "BUY NOW";
+    document.getElementById("buynow2").innerHTML = "BUY NOW";
+    document.getElementById("buynow3").innerHTML = "BUY NOW";
+    document.getElementById("buynow4").innerHTML = "BUY NOW";
+    document.getElementById("buynow5").innerHTML = "BUY NOW";
+    document.getElementById("buynow6").innerHTML = "BUY NOW";
+    document.getElementById("buynow7").innerHTML = "BUY NOW";
+    document.getElementById("buynow8").innerHTML = "BUY NOW";
+    document.getElementById("buynow9").innerHTML = "BUY NOW";
+    document.getElementById("buynow10").innerHTML = "BUY NOW";
+    document.getElementById("buynow11").innerHTML = "BUY NOW";
+    document.getElementById("buynow12").innerHTML = "BUY NOW";
+
+
+}
+function laugugevn() {
+    document.getElementById("home").innerHTML = "Trang chủ";
+    document.getElementById("store").innerHTML = "Cửa hàng";
+    document.getElementById("about").innerHTML = "Về chúng tôi";
+    document.getElementById("servies").innerHTML = "Dịch vụ";
+    document.getElementById("addres").innerHTML = "55 Tô Giang, Quận 1, Thành phố hồ Chí Minh";
+
+    document.getElementById("buynow").innerHTML = "MUA NGAY";
+    document.getElementById("buynow1").innerHTML = "MUA NGAY";
+    document.getElementById("buynow2").innerHTML = "MUA NGAYW";
+    document.getElementById("buynow3").innerHTML = "MUA NGAY";
+    document.getElementById("buynow4").innerHTML = "MUA NGAY";
+    document.getElementById("buynow5").innerHTML = "MUA NGAY";
+    document.getElementById("buynow6").innerHTML = "MUA NGAYW";
+    document.getElementById("buynow7").innerHTML = "MUA NGAY";
+    document.getElementById("buynow8").innerHTML = "MUA NGAY";
+    document.getElementById("buynow9").innerHTML = "MUA NGAY";
+    document.getElementById("buynow10").innerHTML = "MUA NGAY";
+    document.getElementById("buynow11").innerHTML = "MUA NGAY";
+    document.getElementById("buynow12").innerHTML = "MUA NGAY";
+}
+
+function themeBlack() {
+    document.getElementById('container').style.backgroundColor = 'black';
+
+}
+function themeGray() {
+    document.getElementById('container').style.backgroundColor = 'white';
+
+}
