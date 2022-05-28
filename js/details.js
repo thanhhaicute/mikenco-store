a5025d77dc2a76370498c8897f04c2cb26c20ec1
Derry <thanhhaivevo@gmail.com>
2022-05-28 09:52:31 +0700

    UODATE

--- js/details.js
+++ js/details.js
@@ -4,12 +4,7 @@ function laugugeus() {
     document.getElementById("about").innerHTML = "About us";
     document.getElementById("servies").innerHTML = "Service";
     document.getElementById("addres").innerHTML = "55 To Giang, District 1, Ho Chi Minh City";
-    document.getElementById("plus").innerHTML = "Total money:";
-    document.getElementById("update").innerHTML = "UPDATE";
-    document.getElementById("buy").innerHTML = "CONTINUE SHOPPING";
-    document.getElementById("price").innerHTML = "PAY NOW";
-    document.getElementById("title").innerHTML = "Your cart";
-    document.getElementById("des").innerHTML = "There are 2 products in the cart";
+
 
 }
 function laugugevn() {
@@ -18,12 +13,7 @@ function laugugevn() {
     document.getElementById("about").innerHTML = "Về chúng tôi";
     document.getElementById("servies").innerHTML = "Dịch vụ";
     document.getElementById("addres").innerHTML = "55 Tô Giang, Quận 1, Thành phố hồ Chí Minh";
-    document.getElementById("plus").innerHTML = "Tổng tiền:";
-    document.getElementById("update").innerHTML = "CẬP NHẬT";
-    document.getElementById("buy").innerHTML = "TIẾP TỤC MUA HÀNG";
-    document.getElementById("price").innerHTML = "THANH TOÁN";
-    document.getElementById("title").innerHTML = "Giỏ hàng của bạn";
-    document.getElementById("des").innerHTML = "Có 2 sản phẩm trong giỏ hàng";
+
 
 }
 
